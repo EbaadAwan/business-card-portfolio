243350fca169b21e328df7bb61bb8d38d85e3d72
EbaadAwan <ebaadawan213@gmail.com>
2026-07-07 00:07:44 -0400

    second commit with all code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <a href='https://github.com/EbaadAwan' target="_blank"><i class="fa-brands fa-square-github"></i></a>
+        </footer>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Ebaad Awan</h1>
+            <h2>Aspiring Fullstack Developer</h2>
+            <a href='https://www.linkedin.com/in/ebaad-awan-578360299/' target='_blank'> <i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+
+            <section className="about">
+                <h3>About</h3>
+                <p>I am a third-year Computer Science student at Ontario Tech University with a strong interest in full‑stack development. I actively build my skills across both frontend and backend technologies, staying current with modern practices and continuously expanding my knowledge. I'm always eager to learn, improve, and take on new challenges.</p>
+            </section>
+
+            <section className="interests">
+                <h3>Interests</h3>
+                <p>I love playing a lot of sports such as basketball, softball, pickleball, volleyball. I also love to stay active wether that is going to the gym or swimming. I love trying new food from different cultures. I also like talking to new people and getting to know the different personalities that exist around the world.</p>
+            </section>
+        </main>
+    )
+}
